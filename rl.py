5bf29cb4750a4c7b324fab370939e563c4bafe99
yao <yaozhou.wuhu@gmail.com>
2020-04-14 18:45:47 +0800

    remove comments

--- rl.py
+++ rl.py
@@ -9,7 +9,6 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-# import torchvision.transforms as T
 from collections import deque
 import random
 
@@ -32,21 +31,7 @@ class Net(nn.Module):
             nn.Linear(256, action_num)
         )
 
-        # self.fc1 = nn.Linear(height * width, 500)
-        # self.fc1.weight.data.normal_(0, 0.1)
-
-        # self.fc2 = nn.Linear(width, 100)
-        # self.fc2.weight.data.normal_(0, 0.1)
-
-        # self.out = nn.Linear(100, action_num)
-        # self.out.weight.data.normal_(0, 0.1)
-
     def forward(self, x):
-        # x = self.fc1(x)
-        # x = F.relu(x)
-        # x = self.fc2(x)
-        # x = F.relu(x)
-        # x = self.out(x)
         x = self.layer(x)
 
         return x
@@ -60,18 +45,13 @@ class DQN(object):
         self.eval_net = Net(obs_shape[0], obs_shape[1], action_num)
         self.target_net = Net(obs_shape[0], obs_shape[1], action_num)
         self.action_num = action_num
-
-        #self.learn_step_counter = 0     # 用于 target 更新计时
-        #self.memory_counter = 0         # 记忆库记数
-        #self.memory = np.zeros(MEMORY_CAPACITY)     # 初始化记忆库
         
         self.learn_step = 0
         self.memory = deque()
-        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)    # torch 的优化器
-        self.loss_func = nn.MSELoss()   # 误差公式
+        self.optimizer = torch.optim.Adam(self.eval_net.parameters(), lr=LR)
+        self.loss_func = nn.MSELoss()
 
     def choose_action(self, x):
-        #pdb.set_trace()
         x = x.flatten()
         x = torch.unsqueeze(torch.FloatTensor(x), 0)
         if np.random.uniform() < 0.9:
@@ -89,7 +69,6 @@ class DQN(object):
             self.memory.popleft()
 
     def learn(self):
-        #pdb.set_trace()
         if self.learn_step % TARGET_REPLACE_ITER == 0:
             self.target_net.load_state_dict(self.eval_net.state_dict())
         self.learn_step += 1
@@ -101,16 +80,12 @@ class DQN(object):
         r_batch = np.array([d[2] for d in minibatch])
         s1_batch = np.array([d[3] for d in minibatch])
 
-        #pdb.set_trace()
 
         q_eval = self.eval_net(torch.FloatTensor(s0_batch)).gather(1, torch.tensor(a_batch))
         q_next = self.target_net(torch.FloatTensor(s1_batch)).detach()
         q_target = torch.FloatTensor(r_batch) + GAMMA * q_next.max(1)[0]
         loss = self.loss_func(q_eval.squeeze(1), q_target)
-        #pdb.set_trace()
-        #print('loss: %f' % loss)
 
-        # 计算, 更新 eval net
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
@@ -123,7 +98,6 @@ def keyboard_hook(x):
     global action
     if (x.event_type != 'down' or x.name < '0' or x.name > '9'): return
 
-    #print(x)
     action = int(x.name)
 
 INPUT_FILE = '/Users/yao/develop/ARC/data/training/05f2a901.json'
@@ -137,34 +111,8 @@ print(env.observation_space.shape)
 print(env.action_space.n)
 keyboard.hook(keyboard_hook)
 
-
-
-
-#pdb.set_trace()
-
-
-
-# obs = env.reset()
-# print(obs)
-
-# while True:
-#     env.render()
-#     if (action != None):
-#         print('action : ' + str(action))
-#         obs, reward, done, info = env.step(action)
-#         print(obs)
-#         print(reward)
-#         print(done)
-#         action = None
-#     time.sleep(0.1)
-
-
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
 dqn = DQN(env.observation_space.shape, env.action_space.n)
 s = env.reset()
-#pdb.set_trace()
-# a = dqn.choose_action(s.flatten())
 
 for i_episode in range(400):
     s = env.reset()
@@ -172,7 +120,6 @@ for i_episode in range(400):
         env.render()
         a = dqn.choose_action(s)
         #a = env.action_space.sample()
-        #print('action %d' % a)
 
         s1, r, done, info = env.step(a)
         if (r > 0):
@@ -188,19 +135,5 @@ for i_episode in range(400):
             print('episode success!')
             break
 
-        # 修改 reward, 使 DQN 快速学习
-        # x, x_dot, theta, theta_dot = s_
-        # r1 = (env.x_threshold - abs(x)) / env.x_threshold - 0.8
-        # r2 = (env.theta_threshold_radians - abs(theta)) / env.theta_threshold_radians - 0.5
-        # r = r1 + r2
-
-        # 存记忆
-        #dqn.store_transition(s, a, r, s1)
-
-        #if dqn.memory_counter > MEMORY_CAPACITY:
-        #    dqn.learn() # 记忆库满了就进行学习
-
-        if done:    # 如果回合结束, 进入下回合
+        if done:
             break
-
-        #s = s_
