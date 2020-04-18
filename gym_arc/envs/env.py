e4c8d583ab7005b8a581cb91ff7aa7b7b48c65ef
yao <yaozhou.wuhu@gmail.com>
2020-04-18 17:09:10 +0800

    fix aroud

--- gym_arc/envs/env.py
+++ gym_arc/envs/env.py
@@ -2,7 +2,7 @@ import gym
 from gym import error, spaces, utils
 from gym.utils import seeding
 import numpy as np
-import gym_arc.envs.game as game
+import game
 import json
 
 class MultiTaskARCEnv(gym.Env):
